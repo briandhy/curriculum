9aac721ed98fcd0d56b5435dfa4f061562e9ef98
briandhy <briandhy@hotmail.com>
2015-01-22 16:08:29 -0500

    Update guts.js

--- units/5-javascript/lessons/4-project/starter_code/js/guts.js
+++ units/5-javascript/lessons/4-project/starter_code/js/guts.js
@@ -3,4 +3,24 @@ function divide(x,y)
   return x/y;
 }
 
+function multiply(x,y)
+{
+  return x*y;
+}
+
+function subtract(x,y)
+{
+  return x-y;
+}
+
+function add(x,y)
+{
+  return x+y;
+}
+
+function power(x,y)
+{
+  return Math.pow(x,y);
+}
+
 // You must implement the rest of the functions.
